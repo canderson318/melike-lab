03079fccdf9c1c94616122cc6e46c812a890a255
canderson138 <christianjbanderson@gmail.com>
2026-04-20 14:29:29 -0600

    cleaning ws

--- python_main/src/lib/GlucoseSim.py
+++ python_main/src/lib/GlucoseSim.py
@@ -40,14 +40,14 @@ class GlucoseSim:
         self.Ibolus = Ibolus
         self.K = len(t)
         self.results = None  # this is set after calling .run()
-    
+
     # @staticmethod
     # def calculate_c(a, b, t, tk_m):
     #     dt = t[1] - t[0]
     #     t_minus_tk_m = (t - tk_m)
     #     res = np.sum(np.exp(-a*t_minus_tk_m) - np.exp(-b*t_minus_tk_m)) * dt
     #     return res
-    
+
     @staticmethod
     def meal(Gj, a, b, tj_m, gamma, t_k, t_k1):
         """
@@ -59,14 +59,14 @@ class GlucoseSim:
         # #++ upper bound of integral at t+10/a not inf, 10/a close enough to 0
         # t_grid = np.linspace(tj_m, tj_m + 10/a, 10000)
         # const = GlucoseSim.calculate_const(a, b, t_grid, tj_m)
-        const = (a*b)/(b-a) 
-        
+        const = (a*b)/(b-a)
+
         # dt
         hk = t_k1 - t_k
         term1 = (np.exp(-a*(t_k1 - tj_m)) - np.exp(-gamma*hk) * np.exp(-a*(t_k - tj_m))) / (gamma - a)
         term2 = (np.exp(-b*(t_k1 - tj_m)) - np.exp(-gamma*hk) * np.exp(-b*(t_k - tj_m))) / (gamma - b)
         return (Gj * const) * (term1 - term2)
-    
+
     @staticmethod
     def bolusInsulin(Ij, a, b, tj_ins, gamma, t_k, t_k1, x = 5, z = 300):
         """
@@ -93,7 +93,7 @@ class GlucoseSim:
         term1 = (np.exp(-a*(t_k1 - onset)) - np.exp(-gamma*hk) * np.exp(-a*(t_k - onset))) / (gamma - a)
         term2 = (np.exp(-b*(t_k1 - onset)) - np.exp(-gamma*hk) * np.exp(-b*(t_k - onset))) / (gamma - b)
         return const * Ij * (term1 - term2)
-    
+
     @staticmethod
     def integrate_meal(k, Gmeal, a_meal, b_meal, t, gamma):
         return sum(
@@ -107,20 +107,20 @@ class GlucoseSim:
             GlucoseSim.bolusInsulin(Ibolus[j], a_ins, b_ins, t[j], gamma, t[k], t[k+1], x, z)
             for j in range(k+1) if Ibolus[j] > 0
         )
-    
+
     @staticmethod
     def nextG_raw(Gb, gamma, hk, gk, mk, beta, ik):
         return Gb + np.exp(-gamma * hk) * (gk - Gb) + mk - beta*ik
-    
+
     @staticmethod
     def fluctuation(sigma, gamma, hk):
         return sigma * np.sqrt(1-np.exp(-2*gamma*hk))
-        
+
     @staticmethod
     def nextGk(Gb,hk, gamma, gk, mk,ik,beta, sigma):
         """
-        Evolve current glucose gk to next as a function of reversion to baseline, meal, and bolus insulin. 
-        
+        Evolve current glucose gk to next as a function of reversion to baseline, meal, and bolus insulin.
+
         Gb: Basal Glucose.
         hk: time step from current to next timepoint.
         gamma: BG decay rate (1/min)
@@ -128,7 +128,7 @@ class GlucoseSim:
         mk: integral of meal function up to next timepoint (mg/dl)
         ik: integral of insulin function up to next timepoint (mg/dl)
         beta: scales effect of insulin on blood glucose (mg/(dl·U))
-        sigma: average of random fluctuations 
+        sigma: average of random fluctuations
         """
         XIk =  np.random.normal(0,1)
         # evolution = Gb + np.exp(-gamma * hk) * (gk - Gb) + mk - beta*ik
@@ -144,13 +144,13 @@ class GlucoseSim:
             for k in range(self.K - 1):
                 # dt
                 hk = self.t[k+1] - self.t[k]
-                
+
                 mk_integral = GlucoseSim.integrate_meal(k, self.Gmeal, self.a_meal, self.b_meal, self.t, self.gamma)
                 ik_integral = GlucoseSim.integrate_bolusInsulin(k, self.Ibolus, self.a_ins, self.b_ins, self.t, self.gamma, self.x, self.z)
-                
+
                 # evolve current glucose as function meal and params
                 G[k+1] = self.nextGk(Gb=self.Gb, hk=hk, gamma=self.gamma, gk=G[k], mk=mk_integral, ik = ik_integral, sigma=self.sigma, beta = self.beta)
-            
+
             res.append(G.copy())
         self.results = np.array(res).T  # shape (K, num_sim)
         return self
@@ -163,9 +163,9 @@ class GlucoseSim:
         std_G  = df.std(axis=1)
         if ax is None:
             fig, ax = plt.subplots()
-        ax.plot(mean_G, color='steelblue', label='mean',**kwargs)
-        ax.fill_between(df.index, mean_G - 1*std_G, mean_G + 1*std_G,
-                        alpha=0.3, color='steelblue', label='±1 SD')
+        color = kwargs.pop('color', 'steelblue')
+        ax.plot(self.t,mean_G, label='mean', color=color, **kwargs)
+        ax.fill_between(self.t, mean_G - std_G, mean_G + std_G,
+                        alpha=0.3, color=color, label='±1 SD')
         ax.legend(loc = "upper left", framealpha = 1,bbox_to_anchor=(1.15, 1))
         return ax
-    

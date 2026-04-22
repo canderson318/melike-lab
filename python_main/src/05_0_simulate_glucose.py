c466032d055f494727585d45e5e0522af8fd49ff
canderson138 <christianjbanderson@gmail.com>
2026-04-22 12:02:52 -0600

    fix plotting, try more realistic params

--- python_main/src/05_0_simulate_glucose.py
+++ python_main/src/05_0_simulate_glucose.py
@@ -38,68 +38,92 @@ out_dir.mkdir(exist_ok = True)
 # \\\\
 # \\\\
 
-np.random.seed(1020)
+np.random.seed(123)
 # simulate meals
 K = 500
+blood_vol = 5 * 10
+
 t = np.arange(K)
 num_meals = int(10)
 meals = np.zeros(K)
-meals[np.random.choice(range(K-100), num_meals)] = 500
-blood_vol = 5 * 10
+meals[np.random.choice(range(K-100), num_meals)] = 2000
 Gmeal = np.maximum(meals / blood_vol, .01)
 
 # simulate bolus insulin
-num_Ibolus = 4
-I = np.zeros(K)
-I[np.random.choice(range(K-100), num_Ibolus)] = 100
-Ib = np.maximum(I / blood_vol, .01)
+Ib = np.random.normal(150,3,K) / blood_vol
+Ib = np.maximum(Ib,0)
+num_Ibolus = 15
+Ib[np.random.choice(range(K), K-num_Ibolus, replace = False)] = 0
+
+# simulate basal insulin
+I = np.random.normal(60,1,K) / blood_vol
+I = np.maximum(I,0)
 
 
 # set params
 Gb = 140
-sigma = 10
-gamma = .08
-# a     = .5 + gamma # peaks quickly
-# b     = .3 + gamma # drops towards baseline quickly
-a_meal = .05
-b_meal = .07
-a_ins = .01
-b_ins = .1
-beta = 50
+sigma = 20
+gamma = .035
+a_meal = .01
+b_meal = .03
+a_ins = .05
+b_ins = .06
+beta = 30
 
 # run simulation
-sim = GlucoseSim(t = t, Gb=Gb, sigma=sigma, gamma=gamma, a_meal=a_meal, b_meal= b_meal, Gmeal=Gmeal, Ibolus = Ib, a_ins = a_ins, b_ins = b_ins, beta = beta)
-sim.run(num_sim=5)
+sim = GlucoseSim(t = t, Gb=Gb, sigma=sigma, gamma=gamma, 
+                 a_meal=a_meal, b_meal= b_meal, 
+                 Gmeal=Gmeal, 
+                 Ibasal = I, Ibolus = Ib, a_ins = a_ins, b_ins = b_ins, beta = beta)
 
-params = {x[0]: x[1] for x in sim.__dict__.items() if x[0] not in ['t', 'Gmeal', "Ibolus", 'results']}
+sim.run(num_sim=10)
+
+# sim.plot()
+
+# \\\
+# ––– Plot 
+# \\\
+params = {x[0]: x[1] for x in sim.__dict__.items() if x[0] not in ['t', 'Gmeal', "Ibolus", 'results', "Ibasal"]}
 param_str = ", ".join([f"{x[0]}: {x[1]}" for x in params.items()])
 param_str = param_str.replace("b_meal", "\nb_meal")
 
-fig, ax1 = plt.subplots()
-sim.plot(ax = ax1)
+fig, ax = plt.subplots(4,1,figsize = (15,10))
+# ax[0].plot(t, sim.results)
+sim.plot(ax = ax[0])
+ax[0].set_ylabel("Glucose", color="steelblue")
+ax[0].tick_params(axis='y', labelcolor="steelblue")
+ax[0].legend(bbox_to_anchor=(1.05, 1))
+
+ax[1].bar(t, Gmeal, color='orange', width=1, label='Carbs',alpha = 1)
+ax[1].set_ylabel('Carbs', color="orange")
+ax[1].tick_params(axis='y', labelcolor='orange')
+ax[1].set_xlabel("")
+
+ax[2].plot(t, I, color='green', label='Basal', alpha = .3)
+ax[2].set_ylabel('Basal Insulin', color="green")
+ax[2].tick_params(axis = "y", labelcolor = "green")
+ax[2].legend(bbox_to_anchor=(1.05, 1))
+
+ax[3].bar(t, Ib, color='green', label='Bolus', alpha = .7)
+ax[3].set_ylabel('Bolus Insulin', color="green")
+ax[3].tick_params(axis = "y", labelcolor = "green")
+ax[3].legend(bbox_to_anchor=(1.05, 1))
+
+# [ax[i].xaxis.set_major_locator(plt.MultipleLocator(.5)) for i in range(3)]
+plt.suptitle(f"Simulated Glucose evolution", y=1, fontsize=14)
+plt.tight_layout()
 plt.xlabel("Time")
-ax1.set_ylabel("Glucose", color = "steelblue")
-ax1.tick_params(axis = 'y',  labelcolor = "steelblue")
-ax2 = ax1.twinx()
-ax2.plot(t, Gmeal, color='orange', alpha=0.6, label='Gmeal')
-ax2.set_ylabel('Gmeal (mg/dl)', color = "orange", labelpad = 40)
-ax2.tick_params(axis='y', labelcolor='orange', direction='in', pad=-8)
-for label in ax2.get_yticklabels():                                                                  
-      label.set_horizontalalignment('right')       
-ax2.legend(loc = "upper right", bbox_to_anchor=(1.4, .8))
-ax3 = ax1.twinx()
-ax3.spines['right'].set_position(('outward', 0))
-# ax3.set_ylim((None,None))
-ax3.plot(t, Ib, color='green', alpha=0.6, label='Ibolus')
-ax3.set_ylabel('Ib (mg/dl)', color = "green", labelpad = 25)
-ax3.tick_params(axis = 'y',labelcolor = "green")
-ax3.legend(loc="upper right", bbox_to_anchor=(1.4, .7))
-plt.suptitle(f"Simulated Glucose evolution", y = 1.02, fontsize = 14)
-plt.title(param_str, fontsize = 9)
+
 param_path = "_".join([f"{x[0]}{x[1]}" for x in params.items()])
 plt.savefig(out_dir/f"simple_sim_{param_path}.pdf")
-
 
 with  open(out_dir/"sim.pkl", 'wb') as f:
     pkl.dump(sim,f)
     
+
+#@\\\
+#@\\\
+      # Play around with params
+#@\\\
+#@\\\
+      
